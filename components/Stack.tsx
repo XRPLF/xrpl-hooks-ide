405aafed7e1667b0bc571dadc37d506972d3a52d
Valtteri Karesto <valtteri.karesto@gmail.com>
2021-11-26 00:02:39 +0200

    Removed unused code

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -1,8 +1,5 @@
-import { Children } from "react";
-
 import Box from "./Box";
 import { styled } from "../stitches.config";
-import type * as Stitches from "@stitches/react";
 
 const StackComponent = styled(Box, {
   display: "flex",
