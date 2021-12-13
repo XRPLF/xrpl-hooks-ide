b8596ec7cef953f2736f2710a8be6b5944a3b7fa
Valtteri Karesto <valtteri.karesto@gmail.com>
2021-12-13 17:20:35 +0200

    Add some lint settings

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ module.exports = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.resolve.alias["vscode"] = require.resolve(
       "@codingame/monaco-languageclient/lib/vscode-compatibility"
     );
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
     return config;
   },
 };
