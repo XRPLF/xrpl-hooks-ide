43154ff6d830cc0107da2f257d7c030bf1dd921d
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-03-25 09:41:29 +0200

    Updated hooks templates

--- state/constants/templates.ts
+++ state/constants/templates.ts
@@ -1,10 +1,21 @@
+// export const templateFileIds = {
+//     'starter': '1d14e51e2e02dc0a508cb0733767a914', // TODO currently same as accept
+//     'firewall': 'bcd6d0c0fcbe52545ddb802481ff9d26',
+//     'notary': 'a789c75f591eeab7932fd702ed8cf9ea',
+//     'carbon': '43925143fa19735d8c6505c34d3a6a47',
+//     'peggy': 'ceaf352e2a65741341033ab7ef05c448',
+//     'headers': '9b448e8a55fab11ef5d1274cb59f9cf3'
+// }
+
 export const templateFileIds = {
-    'starter': '1d14e51e2e02dc0a508cb0733767a914', // TODO currently same as accept
-    'firewall': 'bcd6d0c0fcbe52545ddb802481ff9d26',
-    'notary': 'a789c75f591eeab7932fd702ed8cf9ea',
-    'carbon': '43925143fa19735d8c6505c34d3a6a47',
-    'peggy': 'ceaf352e2a65741341033ab7ef05c448',
+    'starter': '1f7d2963d9e342ea092286115274f3e3',
+    'firewall': '70edec690f0de4dd315fad1f4f996d8c',
+    'notary': '3d5677768fe8a54c4f6317e185d9ba66',
+    'carbon': 'a9fbcaf1b816b198c7fc0f62962bebf2',
+    'doubler': '56b86174aeb70b2b48eee962bad3e355',
+    'peggy': 'd21298a37e1550b781682014762a567b',
     'headers': '9b448e8a55fab11ef5d1274cb59f9cf3'
+
 }
 
 export const apiHeaderFiles = ['hookapi.h', 'sfcodes.h', 'hookmacro.h']
