87f10a11b03c51bd129d234a4a20d2a1aefd2324
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-05-09 14:18:23 +0300

    Add switch component and bg color to popover

--- components/Popover.tsx
+++ components/Popover.tsx
@@ -27,6 +27,7 @@ const StyledContent = styled(PopoverPrimitive.Content, {
   fontSize: 12,
   lineHeight: 1,
   color: "$text",
+  background: "$background",
   boxShadow:
     "0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)",
   "@media (prefers-reduced-motion: no-preference)": {
