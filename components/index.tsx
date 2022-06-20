783d832c6d8c20ff21e0940211d17337441df698
Valtteri Karesto <valtteri.karesto@gmail.com>
2022-06-20 23:53:32 +0300

    Remove export for unused component

--- components/index.tsx
+++ components/index.tsx
@@ -12,6 +12,5 @@ export { default as Box } from "./Box";
 export { default as Button } from "./Button";
 export { default as Pre } from "./Pre";
 export { default as ButtonGroup } from "./ButtonGroup";
-export { default as DeployFooter } from "./DeployFooter";
 export * from "./Dialog";
 export * from "./DropdownMenu";
